207a21e855509104d670d403a2285c1c370c6938
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2024-12-16 05:06:20 -0600

    chore: removed packages

--- packages/mdxld/readme.md
+++
@@ -1,3 +0,0 @@
-# mdxldx
-
-Markdown & MDX Parsing w/ Integrated support for YAML Front matter and [YAML-LD Linked Data](https://json-ld.github.io/yaml-ld/spec/)
--- packages/mdxld/src/ast.ts
+++
@@ -1,5 +0,0 @@
-import { mdxldWithAst } from './lib/ast-parser.js'
-import type { MDXLDWithAST } from './types/ast.js'
-
-export { mdxldWithAst }
-export type { MDXLDWithAST }
